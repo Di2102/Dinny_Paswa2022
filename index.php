9d0ecb9f5f5e06fa781a23711b3cf07b9b08e75a
Di2102 <Dinnyamalialestari2102@gmail.com>
2022-11-08 17:14:28 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PASWA 2022</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+
+<body style="background-color:indianred";>
+    <div class="container">
+    <div class="text-center">
+        <br><h1 class="text-center text-white">WELCOM PASWA 2022</h1>
+        <div class="row justify-content-around">
+        <div class="text-center">
+            <br><a class="btn btn-dark" href="login.php">Login</a>
+            <a class="btn btn-dark" href="register.php">Register</a>
+        </div>
+    </div>
+</body>
+
+</html>
